aa0b32c61c1477c236e6e9983650e0ea119ebdb5
369431735 <369431735@qq.com>
2017-03-07 21:49:45 +0800

    修改

--- pub_number/src/main/java/com/chatwet/meau/service/ButtonService.java
+++
@@ -1,9 +0,0 @@
-package com.chatwet.meau.service;
-
-import com.chatwet.until.bean.EntityService;
-
-/**
- * Created by lixing on 2017/2/15.
- */
-public class ButtonService<Button,ButtonDao> extends EntityService {
-}
--- pub_number/src/main/java/com/chatwet/until/bean/EntityDaoInterFace.java
+++
@@ -1,9 +0,0 @@
-package com.chatwet.until.bean;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by lixing on 2017/2/15.
- */
-public interface EntityDaoInterFace<T extends BaseEntity> extends JpaRepository<T,String> {
-}
